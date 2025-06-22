a8a33477dc00612c82ff215bf58f52748eaaaa7f
ronitsuman <ronitsuman59@gmail.com>
2025-06-23 02:33:54 +0530

    updated sign in and register

--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -44,7 +44,7 @@ export default function Signup() {
 
     try {
       setLoading(true)
-      const res = await axios.post('http://localhost:5000/api/auth/register', {
+      const res = await axios.post('https://blook-back.onrender.com/api/auth/register', {
         name,
         email,
         mobile,
